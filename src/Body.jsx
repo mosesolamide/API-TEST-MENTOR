2cbdb8ff2afff76dd12a0825864328cfba8e1d4b
mosesolamide <olamidemoses132@gmail.com>
2025-03-28 13:49:16 +0100

    done

--- src/Body.jsx
+++ src/Body.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Body({country,index,capital,official,region,currencyName,currencySymbol,flag,status}){
     return(
-        <ul className="grid grid-cols-9 items-center gap-2 text-[6px] md:text-[7px] w-full px-4 bg-white my-1 rounded-[2px] h-[40px] shadow">
+        <ul className="grid grid-cols-9 items-center gap-3 text-[6px] sm:text-[7px] md:text-[9px] font-medium w-full px-4 bg-white my-1 rounded-[2px] h-[40px] shadow">
             <li className="font-black text-center">{index}</li>
             <li className="flex justify-center">{country}</li>
             <li className="flex justify-center">{capital}</li>
